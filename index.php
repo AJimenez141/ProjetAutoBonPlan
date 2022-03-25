929d36ba5e2bb324e2ad3af0a57439ee6a972852
Pyrex141 <102180111+AJimenez141@users.noreply.github.com>
2022-03-25 21:52:27 +0100

    Affichage bdd dans tableau
    Ajout d'un tableau peuplé avec les données de la bdd en php
    
    Ajout des commentaires du code "traitement-fichier.php"

--- index.php
+++ index.php
@@ -1,6 +1,8 @@
 <?php
 // On démarre la session (ceci est indispensable dans toutes les pages de notre section membre)
 session_start();
+
+include('connexion.php');
 // On récupère nos variables de session
 if (isset($_SESSION['username']) && isset($_SESSION['password']) && isset($_SESSION['role'])) {
     ?>
@@ -34,16 +36,48 @@ if (isset($_SESSION['username']) && isset($_SESSION['password']) && isset($_SESS
             </div>
             <div id="content-wrap">
 
-                <?php
-                if ($_SESSION['role'] == 'admin') {
-                    ?>
+                <table>
+                    <tr>
+                        <th>Marque</th>
+                        <th>Modèle</th>
+                        <th>Carburant</th>
+                        <th>Date d'arrivage</th>
+                        <th>Fournisseur</th>
+                    </tr>
+
+                    <?php
+                    $req = 'SELECT * FROM vehicule WHERE MONTH(dateArr) = MONTH(CURDATE()) ORDER BY dateArr,marque ASC';
+                    $listeElements = $connexion->query($req);
+
+                    foreach ($listeElements as $data) {
+
+                        $originalDate = $data['dateArr'];
+                        $DateTime = DateTime::createFromFormat('Y-m-d', $originalDate);
+                        $newDate = $DateTime->format('d/m/Y');
+                        ?>
+                        <tr>
+                            <td><?php echo $data['marque'] ?? ''; ?></td>
+                            <td><?php echo $data['modele'] ?? ''; ?></td>
+                            <td><?php echo $data['carburant'] ?? ''; ?></td>
+                            <td><?php echo $newDate ?? ''; ?></td>
+                            <td><?php echo $data['fournisseur'] ?? ''; ?></td>
+                        </tr>
+        <?php
+    }
+    ?>
+
+                </table>
+
+    <?php
+    if ($_SESSION['role'] == 'admin') {
+        ?>
                     <form method="post" enctype="multipart/form-data" action="traitement-fichier.php">
                         <input type="file" name="file" required>
-                        <p><button type="submit" name="submit">Envoyer</button></p>
+                        <p><button type="submit" name="submit">Importer</button></p>
                     </form>
-                    <?php
-                }
-                ?>
+        <?php
+    }
+    ?>
 
             </div>
 
@@ -54,3 +88,4 @@ if (isset($_SESSION['username']) && isset($_SESSION['password']) && isset($_SESS
 } else {
     include('login.php');
 }
+?>
--- traitement-fichier.php
+++ traitement-fichier.php
@@ -7,21 +7,23 @@ if (isset($_FILES['file']['name'])) {
 
     $arr_file = explode('.', $_FILES['file']['name']);
     $extension = end($arr_file);
-
+    
+    //On verifie que l'extension du fichier est compatible
     if ('xlsx' == $extension) {
-
+        //On créer un reader de fichier xlsx
         $reader = new \PhpOffice\PhpSpreadsheet\Reader\Xlsx();
-
+        //Le reader charge le fichier 
         $spreadsheet = $reader->load($_FILES['file']['tmp_name']);
-
+        //On récupère le contenu du fichier dans un array
         $data = $spreadsheet->getActiveSheet()->toArray();
 
+        //Pour chaque élément de l'array
         for ($i = 1; $i < count($data); $i++) {
-
+            //On formate la date correctement pour l'insérer dans la bdd en tant que DATE
             $originalDate = $data[$i][45];
             $DateTime = DateTime::createFromFormat('d/m/Y', $originalDate);
             $newDate = $DateTime->format('Y-m-d');
-
+            //On créer l'array avec toutes les informations nécessaires à l'insertion
             $insert_data = array(
                 ':marque' => $data[$i][6],
                 ':modele' => $data[$i][8],
@@ -29,13 +31,16 @@ if (isset($_FILES['file']['name'])) {
                 ':date' => $newDate,
                 ':fournisseur' => $data[$i][2]
             );
-            $query = "insert into vehicule (marque, modele, carburant, dateArr, fournisseur) values(:marque, :modele, :carburant, :date, :fournisseur)";
-            $statement = $connexion->prepare($query);
+            //Ouverture d'une connexion et préparation de la requête
+            $statement = $connexion->prepare("insert into vehicule (marque, modele, carburant, dateArr, fournisseur) values(:marque, :modele, :carburant, :date, :fournisseur)");
+            //On éxecute la requête d'insertion avec l'array en paramètre
             $statement->execute($insert_data);
         }
+        //On signale à l'utilisateur que l'importation a fonctionné et on le renvoie à la page principale
         echo "<script type='text/javascript'>alert('Importation réussie');</script>";
         echo '<meta http-equiv="refresh" content="0;URL=index.php">';
     } else {
+        //On signale à l'utilisateur que seuls les fichiers xlsx sont pris en charge
         echo "<script type='text/javascript'>alert('Prise en charge de fichiers .xlsx uniquement');</script>";
         echo '<meta http-equiv="refresh" content="0;URL=index.php">';
     }
