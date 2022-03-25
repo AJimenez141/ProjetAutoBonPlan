a8b0cfc2f5de6eb7eb593f0eef324f651444a4fc
Pyrex141 <102180111+AJimenez141@users.noreply.github.com>
2022-03-25 15:04:25 +0100

    Toggle import fichier
    Le bouton d'upload de fichier n'apparaît que si l'utilisateur connecté a le rôle d'admin
    
    Mise à jour du script sql avec les modifications apportées (date en DATE et non en TEXT...)

--- bdd_autobonplan.sql
+++ bdd_autobonplan.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Hôte : 127.0.0.1
--- Généré le : jeu. 24 mars 2022 à 12:35
+-- Généré le : ven. 25 mars 2022 à 15:02
 -- Version du serveur :  10.4.11-MariaDB
 -- Version de PHP : 7.4.6
 
@@ -51,12 +51,12 @@ INSERT INTO `utilisateur` (`idUtil`, `nomUtil`, `mdp`, `role`) VALUES
 --
 
 CREATE TABLE `vehicule` (
-  `idArr` varchar(25) COLLATE utf8_bin NOT NULL,
+  `idArr` int(11) NOT NULL,
   `marque` varchar(25) COLLATE utf8_bin NOT NULL,
   `modele` varchar(25) COLLATE utf8_bin NOT NULL,
   `carburant` varchar(25) COLLATE utf8_bin NOT NULL,
   `dateArr` date NOT NULL,
-  `fournisseur` varchar(25) COLLATE utf8_bin NOT NULL
+  `fournisseur` varchar(25) COLLATE utf8_bin DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 
 --
@@ -84,6 +84,12 @@ ALTER TABLE `vehicule`
 --
 ALTER TABLE `utilisateur`
   MODIFY `idUtil` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT pour la table `vehicule`
+--
+ALTER TABLE `vehicule`
+  MODIFY `idArr` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=207;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
--- index.php
+++ index.php
@@ -3,49 +3,54 @@
 session_start();
 // On récupère nos variables de session
 if (isset($_SESSION['username']) && isset($_SESSION['password']) && isset($_SESSION['role'])) {
-?>
+    ?>
     <html lang="fr">
 
-    <head>
-        <!-- Custom CSS -->
-        <link rel="stylesheet" type="text/css" href="main.css">
+        <head>
+            <!-- Custom CSS -->
+            <link rel="stylesheet" type="text/css" href="main.css">
 
-        <meta charset="UTF-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>Autobonplan - ARRIVAGE</title>
-    </head>
+            <meta charset="UTF-8">
+            <meta http-equiv="X-UA-Compatible" content="IE=edge">
+            <meta name="viewport" content="width=device-width, initial-scale=1.0">
+            <title>Autobonplan - ARRIVAGE</title>
+        </head>
 
-    <body>
+        <body>
 
-        <div class="topnav">
-            <div id="nav-container">
+            <div class="topnav">
+                <div id="nav-container">
 
-                <img class="logo" src="https://communication.autobonplan.com/abp-home/img/Logo_Autobonplan.png" />
+                    <img class="logo" src="https://communication.autobonplan.com/abp-home/img/Logo_Autobonplan.png" />
 
-                <div class="login-container">
-                    <img class="user-icon" src="http://cdn.onlinewebfonts.com/svg/img_574534.png" />
+                    <div class="login-container">
+                        <img class="user-icon" src="http://cdn.onlinewebfonts.com/svg/img_574534.png" />
 
-                    <?php echo '<p>Bienvenue, ' . $_SESSION['username'] . '!</p>' ?>
-                    <a href="./fin-session.php">Se déconnecter</a>
+                        <?php echo '<p>Bienvenue, ' . $_SESSION['username'] . '!</p>' ?>
+                        <a href="./fin-session.php">Se déconnecter</a>
+                    </div>
+
                 </div>
+            </div>
+            <div id="content-wrap">
+
+                <?php
+                if ($_SESSION['role'] == 'admin') {
+                    ?>
+                    <form method="post" enctype="multipart/form-data" action="traitement-fichier.php">
+                        <input type="file" name="file" required>
+                        <p><button type="submit" name="submit">Envoyer</button></p>
+                    </form>
+                    <?php
+                }
+                ?>
 
             </div>
-        </div>
 
-        <div id="content-wrap">
-
-            <form method="post" enctype="multipart/form-data" action="traitement-fichier.php">
-                <input type="file" name="file" required>
-                <p><button type="submit" name="submit">Envoyer</button></p>
-            </form>
-
-        </div>
-
-    </body>
+        </body>
 
     </html>
-<?php
+    <?php
 } else {
     include('login.php');
 }
